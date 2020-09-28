ffd066f4ef5a9fe443330ea14d35947eca6dea27
Cristhian Barboza <cbarboza@geocom.com.uy>
2020-09-28 10:51:51 -0300

    primer semana

--- app/src/main/java/uy/com/cdbp/MainActivity.java
+++ app/src/main/java/uy/com/cdbp/MainActivity.java
@@ -3,6 +3,9 @@ package uy.com.cdbp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMyFAB);
+        miFAB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+            }
+        });
+    }
+
+
 }
